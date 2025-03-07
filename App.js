1095cf85a38650a9ab5c08b515c5a2b79159938f
AmanKrs <imkumarak4@gmail.com>
2025-03-08 01:37:59 +0530

    react fn compoent

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const reactHeading = React.createElement(
-  "h1",  //this called react element
-  { style: { color: "red" }, id: "head1", key: "he1" }, //props
-  "Hello from React from parcel" //content if muliple then we use array
-);
-console.log(reactHeading);
-const reactHeading2 = React.createElement(
-  "h2",
-  { style: { color: "blue" }, id: "head2", key: "he2" },
-  "Hello from React secnd child"
-);
-// console.log(reactHeading);
-const container = React.createElement("div", { id: "con" }, [
-  reactHeading,
-  reactHeading2,
-]);
-
+//react element
 const jsx = (
   <h1 id="title" key="qw12">
     React with jsx
   </h1>
 );
+
+// diff way to write react component without return keyword
+const Title = () => (
+  <h1 id="title" key="qw12">
+    react component without return
+  </h1>
+);
+
+//functional component
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* way to write react element in react component as it is a variable*/}
+      {jsx}
+      {/* way to call or add react component inside react component */}
+      {/* this is called component composition */}
+      <Title /> 
+      {/* way to call react component using {} */}
+      {Title()}
+      <h1>Hello Functional Component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(reactHeading); working only one time
-//reactHeading.props.children = "Welcome to Namaste course from javaScript";
-console.log(container);
-console.log(jsx); 
-//root.render(container);
-root.render(jsx);
-// document.getElementById("root").append(reactHeading);
+
+console.log(jsx);
+root.render(<HeaderComponent />);
