945670b37a026f10026aa3659fd1ff4e8c85bbc0
AmanKrs <imkumarak4@gmail.com>
2025-03-10 01:35:01 +0530

    creating dynamic card value

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 
+const mcDonald = {
+  name: "McDonald's",
+  img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/9/9c9ca5f0-b219-46b5-bbba-ea2594524624_804248.JPG",
+  cusines: ["Burger", "American"],
+  rating: "4.2",
+};
 
 const RestuarantCard = () => {
   return (
     <>
       <div className="card">
-        <img src="" alt="resImg" />
+        <img src={mcDonald.img} alt="resImg" />
         <div className="cardInfo">
-          <h3>McDonald's</h3>
-          <h4>American</h4>
-          <p>4.2 star</p>
+          <h3>{mcDonald.name}</h3>
+          <h4>{mcDonald.cusines}</h4>
+          <p>{mcDonald.rating}star</p>
         </div>
       </div>
     </>
